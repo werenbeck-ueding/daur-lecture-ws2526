afcf849ab7ff0b210323d33fc40b8c2f91d342ae
Solaire-patch <s.werenbeck-ueding@outlook.de>
2025-10-22 16:33:46 +0200

    Expand gas consumption model to different control specifications
    Done:
    Included a model with logarithmized horsepower and one with regular horsepower

--- example_file.R
+++ example_file.R
@@ -1,5 +1,14 @@
-# Create a linear regression model
-model <- lm(mpg ~ log(hp) + cyl,
-            data = mtcars)
-# Print a model summary for "model"
-summary(model)
+# Create a linear regression models relating gas consumption to 
+# horsepower and number of cylinders
+
+# Model 1 not using log of horsepower
+model_1 <- lm(mpg ~ hp + cyl,
+              data = mtcars)
+
+# Model 2 using log of horsepower
+model_2 <- lm(mpg ~ log(hp) + cyl,
+              data = mtcars)
+
+# Print a model summaries for both models
+summary(model_1)
+summary(model_2)
